3cb44569ce29bfe6dd907fcb46beb4cb45ca0d66
patmconnolly <patmconnolly@gmail.com>
2016-10-07 13:37:55 -0500

    Quickly updated GUI

--- MCG/Form1.Designer.cs
+++ MCG/Form1.Designer.cs
@@ -28,57 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.juniorGet = new System.Windows.Forms.Button();
+            this.seniorGet = new System.Windows.Forms.Button();
+            this.schoolsGet = new System.Windows.Forms.Button();
+            this.headerGet = new System.Windows.Forms.Button();
+            this.go = new System.Windows.Forms.Button();
+            this.juniorTest = new System.Windows.Forms.TextBox();
+            this.seniorTest = new System.Windows.Forms.TextBox();
+            this.schoolList = new System.Windows.Forms.TextBox();
+            this.scriptHeader = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // juniorGet
             // 
-            this.button1.Location = new System.Drawing.Point(197, 20);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.juniorGet.Location = new System.Drawing.Point(360, 15);
+            this.juniorGet.Name = "juniorGet";
+            this.juniorGet.Size = new System.Drawing.Size(121, 20);
+            this.juniorGet.TabIndex = 0;
+            this.juniorGet.Text = "Get Junior Test";
+            this.juniorGet.UseVisualStyleBackColor = true;
+            this.juniorGet.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // seniorGet
             // 
-            this.button2.Location = new System.Drawing.Point(197, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.seniorGet.Location = new System.Drawing.Point(360, 44);
+            this.seniorGet.Name = "seniorGet";
+            this.seniorGet.Size = new System.Drawing.Size(121, 20);
+            this.seniorGet.TabIndex = 1;
+            this.seniorGet.Text = "Get Senior Test";
+            this.seniorGet.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // schoolsGet
             // 
-            this.button3.Location = new System.Drawing.Point(197, 78);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.schoolsGet.Location = new System.Drawing.Point(360, 73);
+            this.schoolsGet.Name = "schoolsGet";
+            this.schoolsGet.Size = new System.Drawing.Size(121, 20);
+            this.schoolsGet.TabIndex = 2;
+            this.schoolsGet.Text = "Get Schools List";
+            this.schoolsGet.UseVisualStyleBackColor = true;
+            // 
+            // headerGet
+            // 
+            this.headerGet.Location = new System.Drawing.Point(360, 102);
+            this.headerGet.Name = "headerGet";
+            this.headerGet.Size = new System.Drawing.Size(121, 20);
+            this.headerGet.TabIndex = 3;
+            this.headerGet.Text = "Get Script Header";
+            this.headerGet.UseVisualStyleBackColor = true;
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(360, 131);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(121, 20);
+            this.go.TabIndex = 4;
+            this.go.Text = "BFG";
+            this.go.UseVisualStyleBackColor = true;
+            // 
+            // juniorTest
+            // 
+            this.juniorTest.Location = new System.Drawing.Point(12, 15);
+            this.juniorTest.Name = "juniorTest";
+            this.juniorTest.Size = new System.Drawing.Size(342, 20);
+            this.juniorTest.TabIndex = 5;
+            this.juniorTest.Text = "Select Junior Test to Read Scores";
+            // 
+            // seniorTest
+            // 
+            this.seniorTest.Location = new System.Drawing.Point(12, 45);
+            this.seniorTest.Name = "seniorTest";
+            this.seniorTest.Size = new System.Drawing.Size(342, 20);
+            this.seniorTest.TabIndex = 6;
+            this.seniorTest.Text = "Select Senior Test to Read Scores";
+            // 
+            // schoolList
+            // 
+            this.schoolList.Location = new System.Drawing.Point(12, 71);
+            this.schoolList.Name = "schoolList";
+            this.schoolList.Size = new System.Drawing.Size(342, 20);
+            this.schoolList.TabIndex = 7;
+            this.schoolList.Text = "Select List of Schools";
+            // 
+            // scriptHeader
+            // 
+            this.scriptHeader.Location = new System.Drawing.Point(12, 103);
+            this.scriptHeader.Name = "scriptHeader";
+            this.scriptHeader.Size = new System.Drawing.Size(342, 20);
+            this.scriptHeader.TabIndex = 8;
+            this.scriptHeader.Text = "Select Script Header File";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(495, 165);
+            this.Controls.Add(this.scriptHeader);
+            this.Controls.Add(this.schoolList);
+            this.Controls.Add(this.seniorTest);
+            this.Controls.Add(this.juniorTest);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.headerGet);
+            this.Controls.Add(this.schoolsGet);
+            this.Controls.Add(this.seniorGet);
+            this.Controls.Add(this.juniorGet);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Math Contest Grading Software";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button juniorGet;
+        private System.Windows.Forms.Button seniorGet;
+        private System.Windows.Forms.Button schoolsGet;
+        private System.Windows.Forms.Button headerGet;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.TextBox juniorTest;
+        private System.Windows.Forms.TextBox seniorTest;
+        private System.Windows.Forms.TextBox schoolList;
+        private System.Windows.Forms.TextBox scriptHeader;
     }
 }
 
